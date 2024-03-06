fa7c5dd925452369419cb97154a4892e3018c5fe
charybandoju97 <20015269@mydbs.ie>
2024-03-07 01:22:15 +0530

    backend

---
+++ backend/Models/Model.js
@@ -0,0 +1,12 @@
+const mongoose= require("mongoose");
+
+
+
+const schema=mongoose.Schema({
+    
+});
+
+const model=new mongoose.model("",schema);
+
+
+module.exports =model;
---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const dotenv=require('dotenv').config({path:'.env'});
+
+const cors=require('cors');
+
+const app=express();
+
+
+
+app.listen('2000',"0.0.0.0",()=>{console.log('Server running on port : 2000')});
