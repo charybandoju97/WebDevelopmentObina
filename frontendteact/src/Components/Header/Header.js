0b2b712ee3e01217d6cb2590b0341adc2eaa0e22
charybandoju97 <20015269@mydbs.ie>
2024-03-07 14:03:39 +0530

    front components defines

---
+++ frontendteact/src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header()
+{
+   return (
+    <div className='HeaderContainer'>
+          <div className='HeaderNavigation'>
+
+          </div>
+            <button>Sign Up</button>
+    </div>
+   );
+}
+
+export default Header;
---
+++ frontendteact/src/Pages/LandingPage/Landing.js
@@ -0,0 +1,13 @@
+import Header from '../../Components/Header/Header.js';
+import './Landing.css';
+
+function Landing()
+{
+   return (
+    <div>
+        <Header/>
+    </div>
+   );
+}
+
+export default Landing;
