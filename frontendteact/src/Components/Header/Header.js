632ba9142712f914ed867491a9c11e46c98ea476
charybandoju97 <20015269@mydbs.ie>
2024-03-08 16:37:16 +0530

    Banner created

--- frontendteact/src/Components/Header/Header.js
+++ frontendteact/src/Components/Header/Header.js
@@ -1,11 +1,16 @@
 import './Header.css';
-
+import logo from '../../Assets/logo.svg';
 function Header()
 {
    return (
     <div className='HeaderContainer'>
           <div className='HeaderNavigation'>
-
+                <img src={logo} width='160' height='160'/>
+                <h3>Jobs</h3>
+                <h3>Membership</h3>
+                <h3>Newsletter</h3>
+                <h3>Writers</h3>
+                <h3>Blog</h3>
           </div>
             <button>Sign Up</button>
     </div>
--- frontendteact/src/Pages/LandingPage/Landing.js
+++ frontendteact/src/Pages/LandingPage/Landing.js
@@ -1,11 +1,30 @@
 import Header from '../../Components/Header/Header.js';
 import './Landing.css';
-
+import lines from '../../Assets/backgroundlines.png';
+import women from '../../Assets/woman.webp';
 function Landing()
 {
    return (
-    <div>
+    <div className='LandingContainer'>
         <Header/>
+        <div className='BannerContainer'>
+            <div className='BannerSubOne'>
+                <h1>
+                    Find The Best Job
+                </h1>
+                <h3>
+                We hand-pick paid writing opportunities, including remote,<br/> 
+                freelance, contract, part-time, and full-time.
+                </h3>
+                <button>Subscribe To Job NewsLetter</button>
+                <h6>Get the best writing jobs weekly. Free.</h6>
+            </div>
+            <div className='BannerSubTwo'>
+            <img src={women} width="100%" height="100%"/>
+       
+            </div>
+            
+        </div>
     </div>
    );
 }
